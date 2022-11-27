be76068cbea4bd3f489cf64cf37303a0ce31963a
Limon96 <nicker08@inbox.ru>
2022-11-27 17:41:12 +0300

    Update 0.0.14.3
    Исправлено дублирование специализаций

--- public/catalog/assets/js/specialization.js
+++ public/catalog/assets/js/specialization.js
@@ -9,9 +9,9 @@ $(document).on('click', '.general-checkbox', function(){
     recalcSelectedSubjects();
 });
 
-$(document).on('change', '.tabs_spec_ast input[type=checkbox]', function (e) {
+/*$(document).on('change', '.tabs_spec_ast input[type=checkbox]', function (e) {
     saveSpecializations();
-});
+});*/
 
 $(document).on('click', 'button.btn-save', function(e) {
     saveSpecializations();
@@ -36,6 +36,35 @@ function saveSpecializations(){
         dataType : 'json',
         success : function (json) {
             recalcSelectedSubjects();
+        }
+    });
+}
+$(document).on('change', 'input[name=spec_search]', function(e) {
+    filterSpecialisations($(this).val());
+});
+$(document).on('keyup', 'input[name=spec_search]', function(e) {
+    filterSpecialisations($(this).val());
+});
+
+function filterSpecialisations(str){
+    str = str.trim();
+
+    if (!str) {
+        $('.tab-content_spec .for-filter-search').removeClass('filtered');
+        $('.tab-content_spec .for-filter-search').show();
+        $('.tabs_spec .tab-link').show();
+
+        return;
+    }
+
+    $('.tab-content_spec .for-filter-search').each(function(){
+        if ($(this).text().indexOf(str) <= -1) {
+            $(this).removeClass('filtered');
+            $(this).hide();
+            $(this)
+        } else {
+            $(this).addClass('filtered');
+            $(this).show();
         }
     });
 }
