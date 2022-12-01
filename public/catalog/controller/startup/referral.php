2f8b587b7acc3fa41899a1a261a83db15104644b
Limon96 <nicker08@inbox.ru>
2022-12-01 22:06:50 +0300

    Update 0.0.14.6
    Валидация реферального кода

--- public/catalog/controller/startup/referral.php
+++ public/catalog/controller/startup/referral.php
@@ -3,9 +3,26 @@ class ControllerStartupReferral extends Controller
 {
     public function index()
     {
-        if (isset($this->request->get['ref'])) {
+        if ($this->validate()) {
             $this->session->data['referral_code'] = $this->request->get['ref'];
         }
+    }
+
+    private function validate()
+    {
+        if (!isset($this->request->get['ref'])) {
+            return false;
+        }
+
+        $white_list = [
+            'microgadgets'
+        ];
+
+        if (!in_array($this->request->get['ref'], $white_list)) {
+            return false;
+        }
+
+        return true;
     }
 
 }
