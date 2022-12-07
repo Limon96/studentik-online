cb02c723598f5ac79f92faeb7b62be79365b9050
Limon96 <nicker08@inbox.ru>
2022-12-07 16:11:47 +0300

    Update 0.0.14.7

--- public/catalog/model/account/customer.php
+++ public/catalog/model/account/customer.php
@@ -124,7 +124,7 @@ class ModelAccountCustomer extends Model {
             $mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
 
             $mail->setTo($data['email']);
-            $mail->setFrom($this->config->get('config_mail_smtp_username'));
+            $mail->setFrom($this->config->get('config_mail_smtp_sender_mail'));
             $mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
             $mail->setSubject(sprintf($this->language->get('text_subject'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8')));
             $mail->setHTML($this->load->view('mail/register_approval', $data));
@@ -141,7 +141,7 @@ class ModelAccountCustomer extends Model {
             $mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
 
             $mail->setTo($data['email']);
-            $mail->setFrom($this->config->get('config_mail_smtp_username'));
+            $mail->setFrom($this->config->get('config_mail_smtp_sender_mail'));
             $mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
             $mail->setSubject(html_entity_decode(sprintf(
                 $this->language->get('text_subject'),
