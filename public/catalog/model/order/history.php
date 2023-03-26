a08cb6dc9ff5ebbc49f5d777cb6f03041f6a7b0e
Limon96 <nicker08@inbox.ru>
2023-03-26 17:09:24 +0300

    Update 0.0.16.30

--- public/catalog/model/order/history.php
+++ public/catalog/model/order/history.php
@@ -7,9 +7,9 @@ class ModelOrderHistory extends Model {
         $this->db->query("INSERT INTO " . DB_PREFIX . "order_history SET order_id = '" . $data['order_id'] . "', customer_id = '" . $data['customer_id'] . "', text = '" . $data['text'] . "', date_added = '" . time() . "'");
     }
 
-    public function lastHistoryRecord($data)
+    public function lastHistoryRecord($order_id)
     {
-        $result = $this->db->query("SELECT * FROM " . DB_PREFIX . "order_history WHERE order_id = '" . $data['order_id'] . "' ORDER BY date_added DESC LIMIT 1");
+        $result = $this->db->query("SELECT * FROM " . DB_PREFIX . "order_history WHERE order_id = '" . $order_id . "' ORDER BY date_added DESC LIMIT 1");
 
         return $result->row;
     }
