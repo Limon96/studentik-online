ab2967facd053a17d5c9323c6061d6ac34a8f811
Limon96 <nicker08@inbox.ru>
2023-04-05 17:32:22 +0300

    Update 0.0.16.38
    Ссылки на соц сети

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -130,7 +130,125 @@
                                     </div>
                                 </td>
                             </tr>
-                            <tr><td style="font-size:0px;padding:5px 28px;word-break:break-word" align="left"><table style="float:none;display:block" cellspacing="0" cellpadding="0" border="0" align="left"><tbody><tr><td style="padding:0"><table style="border-radius:3px;width:0" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td style="font-size:0;height:0;vertical-align:middle;width:0">&nbsp;</td></tr></tbody></table></td><td style="vertical-align:middle"><a style="color:#999999;font-size:16px;font-family:'PT Sans','Helvetica Neue',Arial,sans-serif;line-height:26px;text-decoration:none" href="http://s2000041246.go.smtp.send-box.ru/stat/5c64acfa31aca35e686a5fffdbad25a5/proc/aHR0cHM6Ly9zdHVkZW50aWsub25saW5lL2NvbnRhY3Rz" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://s2000041246.go.smtp.send-box.ru/stat/5c64acfa31aca35e686a5fffdbad25a5/proc/aHR0cHM6Ly9zdHVkZW50aWsub25saW5lL2NvbnRhY3Rz&amp;source=gmail&amp;ust=1666950836796000&amp;usg=AOvVaw0TBqdcVZsWJYoC8Se6Aq1z">Мы в социальных сетях </a></td></tr></tbody></table><table style="float:none;display:inline-table" cellspacing="0" cellpadding="0" border="0" align="left"><tbody><tr><td style="padding:4px 4px 4px 0px"><table style="border-radius:3px;width:25px" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td style="font-size:0;height:25px;vertical-align:middle;width:25px"><a style="color:#22638f;text-decoration:none" href="http://s2000041246.go.smtp.send-box.ru/stat/5c64acfa31aca35e686a5fffdbad25a5/proc/aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9zdHVkZW50aWsub25saW5lLw==" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://s2000041246.go.smtp.send-box.ru/stat/5c64acfa31aca35e686a5fffdbad25a5/proc/aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9zdHVkZW50aWsub25saW5lLw%3D%3D&amp;source=gmail&amp;ust=1666950836796000&amp;usg=AOvVaw1C5ZjaBkC7rfxDdNcNO5_A"><img width="25" style="border-radius:3px" src="https://ci6.googleusercontent.com/proxy/2ysvvRP7ERgobsvtLngAKyh35pMpes0-JmxX1eaVMW74_C7DCo9uj9WZC9uZiYV7JcWvUj02xETVw3ZuTQ3BmxrU=s0-d-e1-ft#https://studentik.online/mail_letters/insta.png" height="25" alt="Instagram" class="CToWUd" data-bit="iit"> </a></td></tr></tbody></table></td></tr></tbody></table><table style="float:none;display:inline-table" cellspacing="0" cellpadding="0" border="0" align="left"><tbody><tr><td style="padding:4px"><table style="border-radius:3px;width:25px" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td style="font-size:0;height:25px;vertical-align:middle;width:25px"><a style="color:#22638f;text-decoration:none" href="http://s2000041246.go.smtp.send-box.ru/stat/5c64acfa31aca35e686a5fffdbad25a5/proc/aHR0cHM6Ly92ay5jb20vc3R1ZGVudGlrLm9ubGluZQ==" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://s2000041246.go.smtp.send-box.ru/stat/5c64acfa31aca35e686a5fffdbad25a5/proc/aHR0cHM6Ly92ay5jb20vc3R1ZGVudGlrLm9ubGluZQ%3D%3D&amp;source=gmail&amp;ust=1666950836796000&amp;usg=AOvVaw17fie884xl_-b7hUhw9npJ"><img width="25" style="border-radius:3px" src="https://ci5.googleusercontent.com/proxy/2-fM8FfU_u4qvYIqJbi9N3Ib0lme3KplMbEU9Hvba6pR7pncTNZoZd9c0yyG3q8f5EXKbvmjWFUyZoMIwf2k=s0-d-e1-ft#https://studentik.online/mail_letters/vk.png" height="25" alt="VK.com" class="CToWUd" data-bit="iit"> </a></td></tr></tbody></table></td></tr></tbody></table><table style="float:none;display:inline-table" cellspacing="0" cellpadding="0" border="0" align="left"><tbody><tr><td style="padding:4px"><table style="border-radius:3px;width:25px" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td style="font-size:0;height:25px;vertical-align:middle;width:25px"><a style="color:#22638f;text-decoration:none" href="http://s2000041246.go.smtp.send-box.ru/stat/5c64acfa31aca35e686a5fffdbad25a5/proc/aHR0cHM6Ly90Lm1lL3N0dWRlbnRpa19vbmxpbmU=" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://s2000041246.go.smtp.send-box.ru/stat/5c64acfa31aca35e686a5fffdbad25a5/proc/aHR0cHM6Ly90Lm1lL3N0dWRlbnRpa19vbmxpbmU%3D&amp;source=gmail&amp;ust=1666950836796000&amp;usg=AOvVaw2_XrMc5xLYuY1XEklNtZUr"><img width="25" style="border-radius:3px" src="https://ci6.googleusercontent.com/proxy/_j5P5xJP8sw4um-sKdxf_nrQh-d2BlzKBszFD_G7BswagUBGuLtVZf4NngxKKbkSDs54Hm9ks9EJL9f1MU1W=s0-d-e1-ft#https://studentik.online/mail_letters/tg.png" height="25" alt="Telegram" class="CToWUd" data-bit="iit"> </a></td></tr></tbody></table></td></tr></tbody></table></td></tr>
+                            <tr>
+                                <td style="font-size:0px;padding:5px 28px;word-break:break-word" align="left">
+                                    <table align="left" border="0" cellpadding="0" cellspacing="0"
+                                           role="presentation" style="float:none;display: block;">
+                                        <tbody>
+                                        <tr>
+                                            <td style="padding:0">
+                                                <table border="0" cellpadding="0" cellspacing="0"
+                                                       role="presentation"
+                                                       style="border-radius:3px;width:0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="font-size:0;height:0;vertical-align:middle;width:0">
+
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                            <td style="vertical-align:middle">
+                                                <a href="https://studentik.online/contacts"
+                                                   style="color:#999999;font-size:16px;font-family:'PT Sans','Helvetica Neue',Arial,sans-serif;line-height:26px;text-decoration:none"
+                                                   target="_blank"
+                                                   data-saferedirecturl="https://studentik.online/">
+                                                    Мы в социальных сетях
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table align="left" border="0" cellpadding="0" cellspacing="0"
+                                           role="presentation" style="float:none;display:inline-table">
+                                        <tbody>
+                                        <tr>
+                                            <td style="padding:4px 4px 4px 0px">
+                                                <table border="0" cellpadding="0" cellspacing="0"
+                                                       role="presentation"
+                                                       style="border-radius:3px;width:25px">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="font-size:0;height:25px;vertical-align:middle;width:25px">
+                                                            <a href="https://www.instagram.com/studentik.online/"
+                                                               style="color:#22638f;text-decoration:none"
+                                                               target="_blank"
+                                                               data-saferedirecturl="https://www.instagram.com/studentik.online/">
+                                                                <img height="25"
+                                                                     alt="Instagram"
+                                                                     src="https://studentik.online/mail_letters/insta.png"
+                                                                     style="border-radius:3px" width="25"
+                                                                     class="CToWUd">
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table align="left" border="0" cellpadding="0" cellspacing="0"
+                                           role="presentation" style="float:none;display:inline-table">
+                                        <tbody>
+                                        <tr>
+                                            <td style="padding:4px">
+                                                <table border="0" cellpadding="0" cellspacing="0"
+                                                       role="presentation"
+                                                       style="border-radius:3px;width:25px">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="font-size:0;height:25px;vertical-align:middle;width:25px">
+                                                            <a href="https://vk.com/studentik.online"
+                                                               style="color:#22638f;text-decoration:none"
+                                                               target="_blank"
+                                                               data-saferedirecturl="https://vk.com/studentik.online">
+                                                                <img height="25"
+                                                                     alt="VK.com"
+                                                                     src="https://studentik.online/mail_letters/vk.png"
+                                                                     style="border-radius:3px" width="25"
+                                                                     class="CToWUd">
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table align="left" border="0" cellpadding="0" cellspacing="0"
+                                           role="presentation" style="float:none;display:inline-table">
+                                        <tbody>
+                                        <tr>
+                                            <td style="padding:4px">
+                                                <table border="0" cellpadding="0" cellspacing="0"
+                                                       role="presentation"
+                                                       style="border-radius:3px;width:25px">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="font-size:0;height:25px;vertical-align:middle;width:25px">
+                                                            <a href="https://t.me/studentikonline"
+                                                               style="color:#22638f;text-decoration:none"
+                                                               target="_blank"
+                                                               data-saferedirecturl="https://t.me/studentikonline">
+                                                                <img height="25"
+                                                                     alt="Telegram"
+                                                                     src="https://studentik.online/mail_letters/tg.png"
+                                                                     style="border-radius:3px" width="25"
+                                                                     class="CToWUd">
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
                             </tbody>
                         </table>
                     </div>
