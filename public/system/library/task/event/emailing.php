b15eeedb9a03390758bab861338020ebed3dad9c
Limon96 <nicker08@inbox.ru>
2023-04-25 11:43:37 +0300

    Revert "Update emailing.php"
    This reverts commit db1af4f65d248f8523b80515531c561b5d4b7162.

--- public/system/library/task/event/emailing.php
+++ public/system/library/task/event/emailing.php
@@ -61,8 +61,6 @@ class Emailing {
 
             $medata['customer'] = $tpl->render('tool/customer_no_photo');
 
-            $medata = Emailing::convert_from_latin1_to_utf8_recursively($medata);
-
             $template = new Twig();
 
             foreach ($medata as $k => $v) {
@@ -95,24 +93,6 @@ class Emailing {
             "error" => 'Users unknown'
         ];
 
-    }
-
-    public static function convert_from_latin1_to_utf8_recursively($dat)
-    {
-        if (is_string($dat)) {
-            return utf8_encode($dat);
-        } elseif (is_array($dat)) {
-            $ret = [];
-            foreach ($dat as $i => $d) $ret[ $i ] = self::convert_from_latin1_to_utf8_recursively($d);
-
-            return $ret;
-        } elseif (is_object($dat)) {
-            foreach ($dat as $i => $d) $dat->$i = self::convert_from_latin1_to_utf8_recursively($d);
-
-            return $dat;
-        } else {
-            return $dat;
-        }
     }
 
 }
