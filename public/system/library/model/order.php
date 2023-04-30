4f4565de2929142f994fb48cb8bdc1f68e7ec6b3
Limon96 <nicker08@inbox.ru>
2023-04-30 12:51:13 +0300

    Update order.php

--- public/system/library/model/order.php
+++ public/system/library/model/order.php
@@ -6,7 +6,11 @@ class Order extends Model {
 
     public function get($order_id)
     {
-        $result = $this->db->query("SELECT * FROM `" . DB_PREFIX ."order` WHERE order_id = '" . $order_id . "'");
+        $result = $this->db->query("SELECT *,
+                (SELECT s.name FROM `" .DB_PREFIX  ."section` s WHERE s.language_id = '" . (int)$this->config->get('config_language_id') . "' AND s.section_id = o.section_id) AS `section`,
+                (SELECT sj.name FROM `" .DB_PREFIX  ."subject` sj WHERE sj.language_id = '" . (int)$this->config->get('config_language_id') . "' AND sj.subject_id = o.subject_id) AS `subject`,
+                (SELECT wt.name FROM `" .DB_PREFIX  ."work_type` wt WHERE wt.language_id = '" . (int)$this->config->get('config_language_id') . "' AND wt.work_type_id = o.work_type_id) AS `work_type`
+                 FROM `" . DB_PREFIX ."order` o WHERE o.order_id = '" . $order_id . "'");
 
         if (isset($result->row) && $result->row) {
             return (object)$result->row;
