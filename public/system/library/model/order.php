5022814044079705b1b52c7ccadfd4f85a7dd4f3
Limon96 <nicker08@inbox.ru>
2023-04-30 13:02:47 +0300

    Update order.php

--- public/system/library/model/order.php
+++ public/system/library/model/order.php
@@ -6,7 +6,7 @@ class Order extends Model {
 
     public function get($order_id)
     {
-        $result = $this->db->query("SELECT *,
+        $result = $this->db->query("SELECT o.*,
                 (SELECT s.name FROM `" .DB_PREFIX  ."section` s WHERE s.language_id = '" . (int)$this->config->get('config_language_id') . "' AND s.section_id = o.section_id) AS `section`,
                 (SELECT sj.name FROM `" .DB_PREFIX  ."subject` sj WHERE sj.language_id = '" . (int)$this->config->get('config_language_id') . "' AND sj.subject_id = o.subject_id) AS `subject`,
                 (SELECT wt.name FROM `" .DB_PREFIX  ."work_type` wt WHERE wt.language_id = '" . (int)$this->config->get('config_language_id') . "' AND wt.work_type_id = o.work_type_id) AS `work_type`
